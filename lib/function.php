ad81b84359333d7fd2ce1fdc1238b9c388844d07
langnang <langnang.chen@outlook.com>
2022-03-01 15:40:11 +0800

    lib

--- lib/function.php
+++ lib/function.php
@@ -4,7 +4,7 @@ namespace Langnang;
 
 class lnFunction
 {
-  function get_var_type($var)
+  static function get_var_type($var)
   {
     foreach (['array', 'bool', 'float', 'int', 'nan', 'null', 'string'] as $name) {
       if (call_user_func("is_" . $name, $var)) {
