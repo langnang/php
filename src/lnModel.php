bf4a1d9ce4e5d575749f4e244121790fc83664cb
langnang <langnang.chen@outlook.com>
2022-03-01 18:09:55 +0800

    __set

--- src/lnModel.php
+++ src/lnModel.php
@@ -9,12 +9,6 @@ class lnModel
    */
   function __construct($model, ...$args)
   {
-    foreach ($model as $name => $value) {
-      $this->__set($name, $value);
-    }
-    if (method_exists($this, '__self__construct')) {
-      $this->{'__self__construct'}($model, ...$args);
-    }
     foreach (get_class_vars(__CLASS__) as $name => $default_value) {
       if (is_array($default_value) && is_string($model[$name])) {
         $model[$name] = json_decode($model[$name], true);
@@ -25,6 +19,12 @@ class lnModel
       if (is_int($default_value) && is_string($model[$name])) {
         $model[$name] = (int)$model[$name];
       }
+    }
+    foreach ($model as $name => $value) {
+      $this->__set($name, $value);
+    }
+    if (method_exists($this, '__self__construct')) {
+      $this->{'__self__construct'}($model, ...$args);
     }
   }
   /**
@@ -38,7 +38,22 @@ class lnModel
     } else if (!property_exists($this, $name)) {
       return;
     } else {
-      $this->{$name} = $value;
+      $default_value = get_class_vars(__CLASS__)[$name];
+      // 如果为null 或未设置默认值
+      if (is_null($default_value)) {
+        $this->{$name} = $value;
+      } else {
+        if (lnFunction::get_var_type($value) == lnFunction::get_var_type($default_value)) {
+          $this->{$name} = $value;
+        } else {
+          switch (lnFunction::get_var_type($value)) {
+            case 'string':
+              break;
+            default:
+              break;
+          }
+        }
+      }
     }
   }
   /**
